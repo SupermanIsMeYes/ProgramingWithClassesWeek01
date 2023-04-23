032d49e0176df9fb37368e1e7fabadc81963ed66
SupermanIsMeYes <112033629+SupermanIsMeYes@users.noreply.github.com>
2023-04-22 22:48:49 -0600

    HW02

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,13 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name? ");
-        string FirstName = Console.ReadLine();
-        Console.Write("What is your last name?");
-        string LastName = Console.ReadLine();
+        Console.Write("What was your grade? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        string pass = "pass";
+        string letter = "F";
+        
+        if (grade >= 90)
+        {
+            //Console.WriteLine("Your grade is an A");
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            //Console.WriteLine("Your grade is an B");
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            //Console.WriteLine("Your grade is an C");
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            //Console.WriteLine("Your grade is an D");
+            letter = "D";
+            pass = "fail";
+        }
+        else
+        {
+            Console.WriteLine("Your grade is an F");
+            pass = "fail"
+        }
 
-        Console.Writeline();
-        Console.Write($"Your name is {LastName}, {FirstName} {LastName}.");
-
+        Console.WriteLine($"Your grade is an {letter}");
+        
+        if (pass = "pass")
+        {
+            Console.WriteLine("Congrats you pass!!");
+        }
+        else 
+        {
+            Console.WriteLine("Better luck next time you fail.");
+        }
     }
 }
