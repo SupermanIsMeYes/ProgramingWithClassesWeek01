25656e4d5b6506324a1cc9e00f58ac7d7c56a5e8
SupermanIsMeYes <supermanismebvo123@gmail.com>
2023-04-27 01:56:17 -0700

    perp04

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        bool continueIt = true;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+
+        while (continueIt)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number != 0) 
+            { 
+                numbers.Add(number);
+            }
+
+            if (number == 0)
+            {
+                continueIt = false;
+                Console.WriteLine();
+            }
+        }
+        
+        double average = numbers.Count > 0 ? numbers.Average() : 0.0;
+        Console.WriteLine($"The sum is: {numbers.Sum(x => Convert.ToInt32(x))}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+
     }
 }
