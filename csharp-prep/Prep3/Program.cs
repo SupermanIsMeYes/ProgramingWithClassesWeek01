d6c77e99dbb52effc00b3a6d8aaa7e5c4dea759e
SupermanIsMeYes <supermanismebvo123@gmail.com>
2023-04-26 20:51:12 -0700

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool continueIt = true;
+        // Console.Write("What is the magic number? ");
+        // string input = Console.ReadLine();
+        // int magicNumber = Convert.ToInt32(input);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+
+        while (continueIt)
+        {
+            bool numberCorrect = false;
+            Console.Write("What is your guess? ");
+            string input2 = Console.ReadLine();
+            int guess = Convert.ToInt32(input2);
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            if (magicNumber == guess)
+            {
+                Console.WriteLine("You guessed it!");
+                numberCorrect = true;
+            }
+            
+            if (numberCorrect)
+            {
+                continueIt = false;
+            }
+            
+
+}
     }
 }
