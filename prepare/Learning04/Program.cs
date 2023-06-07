a403f3fc8d2c55b91ef33ba4b35957aeacd3549a
SupermanIsMeYes <supermanismebvo123@gmail.com>
2023-06-07 16:34:17 -0600

    hi

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,11 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a base "Assignment" object
         Assignments new1 = new Assignments("Samuel Bennett", "Multiplication");
         Console.WriteLine(new1.GetSummary());
 
-        // Now create the derived class assignments
         math new2 = new math("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(new2.GetSummary());
         Console.WriteLine(new2.GetHomework());
